d4b2861d1e109d961dcc37e04aff6367b7077045
equt <17521736+equt@users.noreply.github.com>
2023-02-03 10:25:01 +0800

    docs: add doc for `get`

--- src/cell.ts
+++ src/cell.ts
@@ -1,10 +1,19 @@
 import { CellAddress, CellObject, Range, utils, WorkSheet } from 'xlsx'
 
+/**
+ * Cell metadata, including the address and the type
+ *
+ * - `address` will always be the real {@link CellAddress}, even for a merged cell
+ *   - `type` indicates if the cell value is retrieved from the the hero of the merged cells
+ * */
 export type CellMeta = Readonly<{
   address: CellAddress
   type: 'DIRECT' | 'MERGED'
 }>
 
+/**
+ * Intermediate cell type
+ * */
 export type Cell = Readonly<
   {
     meta: CellMeta
@@ -56,17 +65,34 @@ const normalize = (
   }
 }
 
+/**
+ * Whether an address is within a {@link Range}
+ * */
 export const contains = (address: CellAddress) => (scope: Range) =>
   address.r >= scope.s.r &&
   address.r <= scope.e.r &&
   address.c >= scope.s.c &&
   address.c <= scope.e.c
 
+/**
+ * Create an array iterating over the {@link Range}, row by row, column by column
+ * */
 export const iterate = (scope: Range): Array<CellAddress> =>
   range(scope.s.r, scope.e.r + 1)
     .product(range(scope.s.c, scope.e.c + 1))
     .map(([r, c]) => ({ r, c }))
 
+/**
+ * Retrieve the {@link Cell} from the {@link WorkSheet}
+ *
+ * Different from the directly indexing, this function will take the `!merges` in the {@link WorkSheet} into account.
+ * If the cell address is not found while within a merged range, the first (and hopefully the only) cell with value will be popped up instead.
+ * Whether it's a direct read or a recursive search could be telled by looking at the `type` in {@link CellMeta}.
+ *
+ * This function will also treat the original `e` and `z`, a.k.a., the error and the empty cell as `undefined`.
+ *
+ * The complexity of this function is O(mn) where m is the size of `!merges`, and n is the size of the largest merged cells group.
+ * */
 export const get = (
   worksheet: WorkSheet,
   address: CellAddress,
